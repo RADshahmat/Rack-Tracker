b3e5a8862188a59a08254c5b46482c81484cea29
RADShahmat <radshahmat1998@gmail.com>
2026-06-16 22:57:20 +0600

    feat(Prometheus): added prometheus scrapper, alert manager,node exporter

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,15 +6,18 @@ import adminRoutes from './admin.routes';
 import warningRoutes from './warning.route';
 import { authMiddleware } from '../middleware/authMiddleware';
 import { casbinMiddleware } from '../middleware/casbinMiddleware';
+import alertRoutes from './alert.routes';
 
 const router = Router();
 
 // Public
 router.use('/auth', authRoutes);
+router.use('/alerts', alertRoutes);
 
 // Protected — auth + authz on every request
 router.use('/racks', authMiddleware, casbinMiddleware, rackRoutes);
 router.use('/equipment', authMiddleware, casbinMiddleware, equipmentRoutes);
 router.use('/warnings', authMiddleware, casbinMiddleware, warningRoutes);
 router.use('/admin', authMiddleware, casbinMiddleware, adminRoutes);
+
 export default router;
