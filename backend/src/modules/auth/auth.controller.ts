f036cc793d8b4727431812de4574d9b3b20b9fe4
RADShahmat <radshahmat1998@gmail.com>
2026-06-21 03:44:25 +0600

    fix(cookie): set samesite none for production

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -23,7 +23,7 @@ class AuthController {
             res.cookie('token', token, {
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                sameSite: 'strict',
+                sameSite: 'none',
                 maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
             });
 
